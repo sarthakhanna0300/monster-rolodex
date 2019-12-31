1745305ef637d379bac357dd7cccddb87c96612c
Sarthak Khanna <sarthaksk6@gmail.com>
2019-12-31 16:04:20 +0530

    adding files for github pages

---
+++ build/precache-manifest.8c6ac0a15002335eca30ea4ed8057805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7e0b1b4722053473712caf772a7d4b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0538d995d8e6315ac636",
+    "url": "/monster-rolodex/static/css/main.031598a7.chunk.css"
+  },
+  {
+    "revision": "72e57426f2a50eae9509",
+    "url": "/monster-rolodex/static/js/2.c710127d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.c710127d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0538d995d8e6315ac636",
+    "url": "/monster-rolodex/static/js/main.c45e60cc.chunk.js"
+  },
+  {
+    "revision": "34cc0ee46c09165226f7",
+    "url": "/monster-rolodex/static/js/runtime-main.0c1ff8b6.js"
+  }
+]);
